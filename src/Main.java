3e9969af23d67a5a5ce4c0de29fb96344837fd11
Kelly Click <kclick91@gmail.com>
2023-08-31 14:22:43 -0400

    83123

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.util.Scanner;
 
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
@@ -33,6 +34,27 @@ public class Main {
         System.out.println(responseTwo.body());
         //Starting my own code
         System.out.println(response.headers());
+
+
+
+        String resOne = response.toString();
+        String restTwo = responseTwo.toString();
+        //JSON Parsing coming
+        //JSONParser parser = new JSONParser();
+
+        //Where MLB starts
+        System.out.println("STARTING AT MLB");
+        int location = responseTwo.body().indexOf("baseball_mlb");
+
+        //Location of home teams
+        int homeLocation = responseTwo.body().indexOf("home_team", location + 1);
+        int homeLocationTwo = responseTwo.body().indexOf("home_team", homeLocation + 1);
+
+        System.out.println(responseTwo.body().substring(location, responseTwo.body().length()));
+        System.out.println(homeLocation);
+        System.out.println(homeLocationTwo);
+
+
 
     }
 }
